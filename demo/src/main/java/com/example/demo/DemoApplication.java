8bb366a6b7be4095bfd8fce85875eaf65c9b64f4
NGW <masaki.nakaigawa@digika.jp>
2024-05-26 07:02:43 +0000

    Test commit.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	/* test */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
